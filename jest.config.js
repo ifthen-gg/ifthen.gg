10ff5024a122b2ab672078de1e5ffdaed12422ee
phlgr <philipp@gartz.dev>
2021-04-05 11:48:20 +0200

    ➕ Install jest with react-testing-library

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
+  transform: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/fileTransformer.js",
+    "\\.(ts|tsx|js|jsx)$": "babel-jest",
+  },
+};
