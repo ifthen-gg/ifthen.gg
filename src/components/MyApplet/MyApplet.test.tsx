da9e9dd24793c8e56a117d08838596735143b15d
lmachens <leon@machens.koeln>
2021-04-17 15:12:11 +0200

    🚨 Stop testing stories due to typing issues

--- src/components/MyApplet/MyApplet.test.tsx
+++ src/components/MyApplet/MyApplet.test.tsx
@@ -1,23 +1,18 @@
 import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
-import { Active, InActive, DifferentColor } from "./MyApplet.stories";
-
-it("renders the Active story", () => {
-  render(<Active {...Active.args} />);
-});
-
-it("renders the InActive story", () => {
-  render(<InActive {...InActive.args} />);
-});
-
-it("renders the DifferentColor story", () => {
-  render(<DifferentColor {...DifferentColor.args} />);
-});
+import MyApplet from "./MyApplet";
 
 it("calls active event on change", () => {
   const handleActiveChange = jest.fn();
 
-  render(<Active {...Active.args} onActiveChange={handleActiveChange} />);
+  render(
+    <MyApplet
+      active
+      color="blue"
+      text="If CPU usage is high, then close Chrome."
+      onActiveChange={handleActiveChange}
+    />
+  );
   fireEvent.click(screen.getByRole("checkbox"));
 
   expect(handleActiveChange).toHaveBeenCalledWith(false);
@@ -26,7 +21,14 @@ it("calls active event on change", () => {
 it("calls inactive event on change", () => {
   const handleActiveChange = jest.fn();
 
-  render(<InActive {...InActive.args} onActiveChange={handleActiveChange} />);
+  render(
+    <MyApplet
+      active={false}
+      color="blue"
+      text="If CPU usage is high, then close Chrome."
+      onActiveChange={handleActiveChange}
+    />
+  );
   fireEvent.click(screen.getByRole("checkbox"));
 
   expect(handleActiveChange).toHaveBeenCalledWith(true);
