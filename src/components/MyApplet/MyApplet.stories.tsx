4c9df2b2347d80b7d3993f2d2b7a66d95389fd3a
lmachens <leon@machens.koeln>
2021-04-17 16:14:37 +0200

    ✅ Test stories

--- src/components/MyApplet/MyApplet.stories.tsx
+++ src/components/MyApplet/MyApplet.stories.tsx
@@ -5,28 +5,33 @@ import MyApplet, { MyAppletProps } from "./MyApplet";
 export default {
   title: "Common/MyApplet",
   component: MyApplet,
+  excludeStories: /.*Args$/,
 } as Meta;
 
+type MyAppletPropsWithoutEvents = Omit<MyAppletProps, "onActiveChange">;
 const Template: Story<MyAppletProps> = (args) => <MyApplet {...args} />;
 
 export const Active = Template.bind({});
-Active.args = {
+export const activeArgs: MyAppletPropsWithoutEvents = {
   active: true,
   color: "#2D89A1",
   text: "If CPU usage is high, then close Chrome.",
 };
+Active.args = activeArgs;
 
-export const InActive = Template.bind({});
-InActive.args = {
+export const Inactive = Template.bind({});
+export const inactiveArgs: MyAppletPropsWithoutEvents = {
   active: false,
   color: "#2D89A1",
   text: "If CPU usage is high, then close Chrome.",
 };
+Inactive.args = inactiveArgs;
 
 export const DifferentColor = Template.bind({});
-DifferentColor.args = {
+export const differentColorArgs: MyAppletPropsWithoutEvents = {
   active: true,
   color: "#784A8D",
   text:
     'If League of Legends game is starting, then set Status on Slack to "In a Meeting".',
 };
+DifferentColor.args = differentColorArgs;
