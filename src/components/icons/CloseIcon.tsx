b7bb00de10fdefbf1d18d9558659f1937dc6f3e8
lmachens <leon@machens.koeln>
2021-05-01 15:52:15 +0200

    🔥 Remove unused fill-rule

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -13,7 +13,6 @@ function CloseIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element {
       <path
         d="M10.146,10.146a.5.5,0,0,1,.707,0L15,14.293l4.146-4.146a.5.5,0,0,1,.707.707L15.707,15l4.146,4.146a.5.5,0,0,1-.707.707L15,15.707l-4.146,4.146a.5.5,0,0,1-.707-.707L14.293,15l-4.146-4.146A.5.5,0,0,1,10.146,10.146Z"
         transform="translate(-10 -10)"
-        fillRule="evenodd"
       />
     </svg>
   );
