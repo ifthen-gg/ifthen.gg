2f8e1f0cd48881ea17c41c77f9fad002bfa88ddc
lmachens <leon@machens.koeln>
2021-05-06 20:30:02 +0200

    🏷️ Add Overwolf types

--- src/background.tsx
+++ src/background.tsx
@@ -1,2 +1,7 @@
 console.log("Starting background process");
+
+overwolf.windows.obtainDeclaredWindow("desktop", (result) => {
+  overwolf.windows.restore(result.window.id, console.log);
+});
+
 export {};
