8a32c3641f1c7776f34b06e9630fff67041413ac
lmachens <leon@machens.koeln>
2021-05-24 18:41:19 +0200

    🏷️ Add InfoUpdateEvent

--- src/triggers/leagueQueue.ts
+++ src/triggers/leagueQueue.ts
@@ -4,13 +4,26 @@ export const LEAGUE_QUEUE = "leagueQueue";
 const LEAGUE_LAUNCHER_ID = 10902;
 const REQUIRED_FEATURES = ["lobby_info"];
 
+// Overwolf doesn't have types for onInfoUpdates. This should work fine for now.
+// It's based on https://overwolf.github.io/docs/api/overwolf-games-launchers-events-lol#lobby_info
+type InfoUpdateEvent = {
+  feature: "lobby_info";
+  info: {
+    lobby_info: {
+      queueId: string;
+    };
+  };
+};
+
 const leagueQueue: Trigger<string> = {
   name: LEAGUE_QUEUE,
   title: "League Queue",
   description: "This triggers when League of Legends queue changes",
   listen(callback) {
-    const handleInfoUpdates = (res: any) => {
-      callback(res.info.lobby_info.queueId);
+    const handleInfoUpdates = (res: InfoUpdateEvent) => {
+      if (res.feature === "lobby_info") {
+        callback(res.info.lobby_info.queueId);
+      }
     };
 
     overwolf.games.launchers.events.setRequiredFeatures(
